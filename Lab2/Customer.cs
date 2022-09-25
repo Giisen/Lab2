45c4538a8f4140252b2afa524a0363e7e4ef31e9
giisen <kristerekelund@me.com>
2022-09-25 11:58:21 +0200

    1

--- Lab2/Customer.cs
+++ Lab2/Customer.cs
@@ -8,5 +8,26 @@ public class Customer
         get { return _CustomerName; }
         set { _CustomerName = value; }
     }
+
+    private string _CustomerPassword;
+    public string CustomerPassword
+    {
+        get { return _CustomerPassword; }
+        set { _CustomerPassword = value; }
+    }
+
+    public Customer(string name, string password)
+    {
+        CustomerName = name;
+        CustomerPassword = password;
+        
+    }
+
+    public void ToString()
+    {
+        Console.WriteLine($"Ditt namn : {CustomerName}");
+        Console.WriteLine($"Ditt lösenord är: {CustomerPassword}");
+        Console.WriteLine($"Din kunvagn består just nu av: ...Kunvagn.....");
+    }
 }
 
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Lab2;
+Console.WriteLine("Välkomen till butiken -* GIISEN *-\n");
+
+Customer Kund1 = new Customer("Knatte", "123");
+Customer Kund2 = new Customer("Fnatte", "321");
+Customer Kund3 = new Customer("Tjatte", "213");
+
+
+bool meny1 = true;
+while (meny1)
+{
+    Console.WriteLine("\nVälj 1 om du är ny kund och vill skapa en profil\n" +
+                      "\nVälj 2 om du redan är kund och vill logga in");
+    string inputMeny1 = Console.ReadLine();
+
+    switch (inputMeny1)
+    {
+        case "1":
+            Console.WriteLine("Skriv in ditt användarnamn: ");
+            string newCustomerName = Console.ReadLine();
+            Console.WriteLine("Skriv in ditt lösenord: ");
+            string newCustomerPassword = Console.ReadLine();
+            Customer Kund4 = new Customer(newCustomerName, newCustomerPassword); //Här skulle jag vilja ha ett annat namn på Kund4, ex newCustomerName
+            meny1 = false;
+            break;
+
+        case "2":
+            Console.WriteLine("Skriv in ditt användarnamn: ");
+            string CustomerName = Console.ReadLine();
+            Console.WriteLine("Skriv in ditt lösenord: ");
+            string CustomerPassword = Console.ReadLine();
+            meny1 = false;
+            break;
+
+        default:
+            Console.WriteLine("Du gjorde ett ogiltigt val, välj 1 eller 2");
+            meny1 = true;
+            break;
+    }
+}
